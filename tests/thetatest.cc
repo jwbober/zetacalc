79b42a52fbd96e0e85235dfe20e10426fd0aa9f0
Jonathan Bober <jwbober@gmail.com>
2015-05-02 23:38:46 +0100

    multithreaded testing code.

--- tests/thetatest.cc
+++ tests/thetatest.cc
@@ -3,13 +3,20 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <thread>
+#include <mutex>
 
 #include <ctime>
 #include <cstdlib>
 #include <cmath>
 #include <complex>
+#include <random>
 
 #include "theta_sums.h"
+
+typedef std::mt19937 RNG;
+std::uniform_real_distribution<double> random01(0, 1);
+
 
 using std::max;
 using std::stringstream;
@@ -20,149 +27,55 @@ using std::endl;
 //using std::log2;
 using std::complex;
 
-inline double random_double(const double start = 0, const double end = 1) {
-    return ((double)std::rand()/((double)RAND_MAX + 1.0)) * (end - start) + start;
+
+inline double random_double(RNG &rng, const double start = 0, const double end = 1) {
+    return random01(rng) * (end - start) + start;
 }
 
-inline complex<double> random_complex(const double start = 0, const double end = 1) {
-    return complex<double>(random_double(start, end), random_double(start, end));
+inline complex<double> random_complex(RNG &rng, const double start = 0, const double end = 1) {
+    return complex<double>(random_double(rng, start, end), random_double(rng, start, end));
 }
 
-inline int random_int(const int start, const int end) {
+inline int random_int(RNG &rng, const int start, const int end) {
     //
     // includes endpoint
     //
-    return (int)random_double(start, end + 1);
+    return (int)random_double(rng, start, end + 1);
 }
 
+const int histogram_size = 60;
+const int histogram_start = 0;
+int error_histogram[histogram_size] = {0};
+complex<double> maxerror = 0.0;
 
-complex<double> test_one(complex<double> * S1,
-                         complex<double> * S2,
-                         double a,
-                         double b,
-                         int j,
-                         int K,
-                         complex<double> * v,
-                         double epsilon ) {
+string largest_error_string;
 
-    *S1 = compute_exponential_sums(a, b, j, K, v, epsilon, 100, 0);
-    *S2 = compute_exponential_sums(a, b, j, K, v, epsilon, 0, 4);
-    
-    cout << "./thetatest one ";
-    cout << a << " ";
-    cout << b << " ";
-    cout << j << " ";
-    cout << K << " ";
-    cout << epsilon << " ";
-    for(int k = 0; k < j; k++) {
-        cout << "\\(" << v[k].real() << "," << v[k].imag() << "\\)" << " ";
+std::mutex report_mutex;
+
+void report(complex<double> error, string test_string) {
+    report_mutex.lock();
+    double logerror = -log2(abs(error));
+    if(logerror < histogram_start) {
+        error_histogram[0]++;
     }
-    cout << "\\(" << v[j].real() << "," << v[j].imag() << "\\)" << endl;
+    else if(logerror >= histogram_size - histogram_start){
+        error_histogram[histogram_size - 1]++;
+    }
+    else {
+        error_histogram[int(floor(logerror)) + histogram_start]++;
+    }
 
-    complex<double> error = *S1 - *S2;
-    cout << "               ERROR: " << error << endl;
-    cout << "         log2(ERROR): " << log2(abs(error)) << endl;
-    cout << "    algorithm answer: " << *S1 << endl;
-    cout << "       direct answer: " << *S2 << endl;
-    cout << endl;
-
-    return error;
+    if(abs(error) > abs(maxerror)) {
+        maxerror = error;
+        largest_error_string = test_string;
+        cout << "Worst so far:" << endl;
+        cout << test_string;
+    }
+    report_mutex.unlock();
 }
 
-int test_theta_algorithm(int number_of_tests,
-                         int approx_K,
-                         double epsilon = pow(2, -29),
-                         bool EM = false,
-                         int run_only = -1) {
+void print_histogram() {
 
-    const int j_max = 18;
-    complex<double> * v = new complex<double>[j_max + 1];
-
-    cout << endl;
-    cout << endl;
-    cout << endl;
-    cout << "Testing theta algorithm with various random parameters ";
-    cout <<         number_of_tests << " times, with log2(epsilon) = " << log2(epsilon) << endl;
-
-    complex<double> maxerror = 0.0;
-    complex<double> S1, S1error;
-    complex<double> S2, S2error;
-    complex<double> error;
-
-    double a,b;
-    
-    const int histogram_size = 60;
-    const int histogram_start = 0;
-    int error_histogram[histogram_size];
-    for(int n = 0; n < histogram_size; n++) {
-        error_histogram[n] = 0;
-    }
-
-    int bad_test_number = -1;
-    for(int n = 0; n < number_of_tests; n++) {
-
-        int K = random_int(approx_K - 500, approx_K + 501);
-        int j = random_int(0, 18);
-    
-        if(EM) {
-            a = random_double();
-            b = random_double()/(2 * K); 
-        }
-        else {
-            a = random_double(-10, 10);
-            b = random_double(-10, 10);
-        }
-
-
-        for(int k = 0; k <= j; k++) {
-            //v[k] = (random_complex(-1, 1) * 2.0 - complex<double>(1.0, 1.0))/(k*k*k*k + 1.0);
-            //v[k] = (random_complex(-1, 1) * 2.0 - complex<double>(1.0, 1.0));
-            v[k] = 1.0;
-        }
-
-        if(run_only == -1 || run_only == n) {
-            cout << "Test number " << n << endl;
-            error = test_one(&S1, &S2, a, b, j, K, v, epsilon);
-            error = S1 - S2;
-
-            double logerror = -log2(abs(S1 - S2));
-            if(logerror < histogram_start) {
-                error_histogram[0]++;
-            }
-            else if(logerror >= histogram_size - histogram_start){
-                error_histogram[histogram_size - 1]++;
-            }
-            else {
-                error_histogram[int(floor(logerror)) + histogram_start]++;
-            }
-
-            if(abs(error) > abs(maxerror)) {
-                bad_test_number = n;
-                maxerror = error;
-                S1error = S1;
-                S2error = S2;
-            }
-
-            
-            //cout    << "Test "  << n
-            //        << ": a = " << a
-            //        << ", b = " << b
-            //        << ", j = " << j
-            //        << ", K = " << K
-            //        << ":                     log2(error) = " << log2(abs(error)) << endl
-            //        << "          error = " << error                              << endl
-            //        << "          answer = " << S2                                << endl;
-        }
-    }
-
-    cout << "Largest error was "    << maxerror                 << endl
-         << "    log2(maxerror) = "   << log2(abs(maxerror))    << endl
-         << "    requested was "  << log2(epsilon)              << endl
-         << "This happened on test number " << bad_test_number  << endl;
-
-    cout << endl;
-    cout << endl;
- 
     int hist_max_entry = 0;
     for(int n = 0; n < histogram_size; n++) {
         hist_max_entry = max(error_histogram[n], hist_max_entry);
@@ -182,6 +95,91 @@ int test_theta_algorithm(int number_of_tests,
         }
         cout << endl;
     }
+}
+
+
+complex<double> test_one(complex<double> * S1,
+                         complex<double> * S2,
+                         double a,
+                         double b,
+                         int j,
+                         int K,
+                         complex<double> * v,
+                         double epsilon,
+                         string &teststring) {
+
+    *S1 = compute_exponential_sums(a, b, j, K, v, epsilon, 100, 0);
+    *S2 = compute_exponential_sums(a, b, j, K, v, epsilon, 0, 4);
+ 
+    stringstream ss;
+    ss << std::setprecision(17);
+    ss << "./thetatest one ";
+    ss << a << " ";
+    ss << b << " ";
+    ss << j << " ";
+    ss << K << " ";
+    ss << epsilon << " ";
+    for(int k = 0; k < j; k++) {
+        ss << "\\(" << v[k].real() << "," << v[k].imag() << "\\)" << " ";
+    }
+    ss << "\\(" << v[j].real() << "," << v[j].imag() << "\\)" << endl;
+
+    complex<double> error = *S1 - *S2;
+    ss << "               ERROR: " << error << endl;
+    ss << "         log2(ERROR): " << log2(abs(error)) << endl;
+    ss << "    algorithm answer: " << *S1 << endl;
+    ss << "       direct answer: " << *S2 << endl;
+    ss << endl;
+
+    teststring = ss.str();
+
+    return error;
+}
+
+int test_theta_algorithm(int number_of_tests,
+                         int approx_K,
+                         double epsilon = pow(2, -29),
+                         bool EM = false,
+                         unsigned int seed = 0) {
+
+    RNG rng;
+    rng.seed(seed);
+
+    const int j_max = 18;
+    complex<double> * v = new complex<double>[j_max + 1];
+
+    complex<double> S1;
+    complex<double> S2;
+    complex<double> error;
+
+    double a,b;
+
+    for(int n = 0; n < number_of_tests; n++) {
+
+        int K = random_int(rng, approx_K - 500, approx_K + 501);
+        int j = random_int(rng, 0, 18);
+    
+        if(EM) {
+            a = random_double(rng);
+            b = random_double(rng)/(2 * K); 
+        }
+        else {
+            a = random_double(rng, -10, 10);
+            b = random_double(rng, -10, 10);
+        }
+
+
+        for(int k = 0; k <= j; k++) {
+            //v[k] = (random_complex(-1, 1) * 2.0 - complex<double>(1.0, 1.0))/(k*k*k*k + 1.0);
+            //v[k] = (random_complex(-1, 1) * 2.0 - complex<double>(1.0, 1.0));
+            v[k] = 1.0;
+        }
+
+        string teststring;
+        error = test_one(&S1, &S2, a, b, j, K, v, epsilon, teststring);
+        error = S1 - S2;
+        report(error, teststring);
+    }
     delete [] v;
     return 0;
 }
@@ -198,7 +196,7 @@ int main(int argc, char ** _argv) {
 
     if(argv[1] == "many") {
         //
-        // expect input in the form seed, number_of_tests, Kapprox, epsilon, EM
+        // expect input in the form seed, nthreads, number_of_tests, Kapprox, epsilon, EM
         //
         // a seed of 0 will mean we use the choice seed = time().
         //
@@ -212,11 +210,12 @@ int main(int argc, char ** _argv) {
             }
         }
 
-        unsigned int seed = 0, number_of_tests = 1000, Kapprox = 1000;
+        unsigned int seed = 0, nthreads = 1, number_of_tests = 1000, Kapprox = 1000;
         bool EM = false;
         double epsilon = pow(2, -30);
 
         ss >> seed;
+        ss >> nthreads;
         ss >> number_of_tests;
         ss >> Kapprox;
         ss >> epsilon;
@@ -226,9 +225,19 @@ int main(int argc, char ** _argv) {
             seed = time(NULL);
         }
         
-        srand(seed);
-        cout << "seeding rand() with " << seed << "." << endl;
-        test_theta_algorithm(number_of_tests, Kapprox, epsilon, EM);
+        cout << "seeding random() with " << seed << "." << endl;
+        if(nthreads == 1) {
+            test_theta_algorithm(number_of_tests, Kapprox, epsilon, EM, seed);
+        }
+        else {
+            std::thread threads[nthreads];
+            for(unsigned int k = 0; k < nthreads; k++) {
+                threads[k] = std::thread(test_theta_algorithm, number_of_tests/nthreads, Kapprox, epsilon, EM, seed + k);
+            }
+            for(unsigned int k = 0; k < nthreads; k++) {
+                threads[k].join();
+            }
+        }
         return 0;
     }
 
@@ -266,7 +275,8 @@ int main(int argc, char ** _argv) {
         }
         complex<double> S1;
         complex<double> S2;
-        test_one(&S1, &S2, a, b, j, K, v, epsilon);
+        string teststring;
+        test_one(&S1, &S2, a, b, j, K, v, epsilon, teststring);
 
         delete [] v;
         return 0;
@@ -280,17 +290,17 @@ int main(int argc, char ** _argv) {
     return 0;
 
     // The following had a bad problem at some point.
-    complex<double> v[19];
-    for(int j = 0; j < 19; j++) {
-        v[j] = 1;
-    }
-    complex<double> S1;
-    complex<double> S2;
+    //complex<double> v[19];
+    //for(int j = 0; j < 19; j++) {
+    //    v[j] = 1;
+    //}
+    //complex<double> S1;
+    //complex<double> S2;
     //complex<double> error = test_one(&S1, &S2, -1.1759932897985, -2.12365078739822, 18, 10414, v, pow(2, -30));
-    complex<double> error = test_one(&S1, &S2, -1.1759932897985, -2.12365078739822, 18, 10352, v, pow(2, -30));
-    cout << error << " ";
-    cout << S1 << " ";
-    cout << S2 << endl;
+    //complex<double> error = test_one(&S1, &S2, -1.1759932897985, -2.12365078739822, 18, 10352, v, pow(2, -30));
+    //cout << error << " ";
+    //cout << S1 << " ";
+    //cout << S2 << endl;
 
     return 0;
 }
